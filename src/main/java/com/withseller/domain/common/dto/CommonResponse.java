d44d399150979002ec2c14faefb2e447f517d918
hmmini <pak4184@naver.com>
2023-11-12 18:02:15 +0900

    [dev] Custom Exception & Custom response body

--- src/main/java/com/withseller/domain/common/dto/CommonResponse.java
+++ src/main/java/com/withseller/domain/common/dto/CommonResponse.java
@@ -1,11 +1,29 @@
 package com.withseller.domain.common.dto;
 
+import com.withseller.exception.ErrorCode;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.http.ResponseEntity;
 
 @Data
 @Builder
 public class CommonResponse {
     private String status;
     private String message;
+
+    public static ResponseEntity<CommonResponse> toResponseEntity(ErrorCode errorCode) {
+        return ResponseEntity.status(errorCode.getStatus())
+                .body(CommonResponse.builder()
+                        .status("FAIL")
+                        .message(errorCode.getMessage())
+                        .build());
+    }
+
+    public static ResponseEntity<CommonResponse> toResponseEntity(ErrorCode errorCode, String message) {
+        return ResponseEntity.status(errorCode.getStatus())
+                .body(CommonResponse.builder()
+                        .status("FAIL")
+                        .message(message)
+                        .build());
+    }
 }
---
+++ src/main/java/com/withseller/exception/CustomException.java
@@ -0,0 +1,10 @@
+package com.withseller.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+}
